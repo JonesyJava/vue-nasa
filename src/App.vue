823a3553bd13b8185e753205cb2f09c4736b2470
Jonesy <jonesyjava@gmail.com>
2021-05-05 13:27:05 -0600

    Changed App.vue - added query and logos

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
-<div class="app">
-  <header class="container-fluid bg-dark text-light">
-    <div class="row">
-      <div class="col text-center">
-        <h1>Welcome to <img alt="Vue logo" src="./assets/logo.png">ue NASA</h1>
-      </div>
-    </div>
-  </header>
-  <main class="container mt-2">
-    <div class="row">
-      <div class="col">
-        <div>
-          <form @submit.prevent="searchApod">
-            <input type="date" v-model="state.query">
-            <button type="submit">Search</button>
-          </form>
-        </div>
-        <div class="mt-3">
-        <APOD />
-        </div>
-      </div>
-    </div>
-  </main>
-  <footer class="container-fluid bg-dark text-light">
+  <div class="app">
+    <header class="container-fluid bg-dark text-light">
       <div class="row">
         <div class="col text-center">
-          <p class="m-3"> Made with ♥ by JONESY</p>
+          <h1>
+            Welcome to <img alt="Vue logo" src="./assets/logo.png" />ue NASA
+          </h1>
+        </div>
+      </div>
+    </header>
+    <main class="container mt-2">
+      <div class="row">
+        <div class="col">
+          <div>
+            <form @submit.prevent="searchApod">
+              <input type="date" v-model="state.query" />
+              <button type="submit">Search</button>
+            </form>
+          </div>
+          <div class="mt-3">
+            <APOD />
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="container-fluid bg-dark text-light">
+      <div class="row">
+        <div class="col text-center">
+          <p class="m-3">Made with ♥ by JONESY</p>
         </div>
       </div>
     </footer>
-</div>
+  </div>
 </template>
 
 <script>
@@ -67,13 +69,13 @@ export default {
   color: #2c3e50;
 }
 
-.app{
+.app {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
 }
 
-main{
+main {
   flex-grow: 1;
 }
 
@@ -81,7 +83,7 @@ h1 img {
   height: 1em;
 }
 
-form{
+form {
   border: 3px solid gray;
   background-color: rgb(65, 184, 131);
   border-radius: 20px;
@@ -91,13 +93,13 @@ form{
   margin-right: 115px;
 }
 
-input{
+input {
   border-radius: 20px;
   box-shadow: 5px 3px 3px black;
   margin-right: 15px;
 }
 
-button{
+button {
   border-radius: 20px;
   box-shadow: 5px 3px 3px black;
 }
